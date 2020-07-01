6a7f40fc0868c556354c6c1ea23271db26d9e0fb
israelpalma <ipalm001@gmail.com>
2020-06-30 21:16:42 -0400

    ejemplo for oares e impares

--- funciones2.py
+++ funciones2.py
@@ -27,3 +27,21 @@ for x in range(var1):
     lista.append(contador) #agregando contenido a la lista
 
 print("tamaño lista es : ",len(lista)) #tamaño de la lista
+
+
+print("********************")
+
+
+#crear una lista con los 5 primeros numeros pares
+
+lista_numeros_pares=[]
+lista_numeros_impares=[]
+#for variable in elemento iterable (range) :
+for i in range(1,11):
+    if(i%2 ==0):
+        lista_numeros_pares.append(i)
+    else:
+        lista_numeros_impares.append(i)
+
+print("numeros pares: ",lista_numeros_pares)
+print("numeros impares: ",lista_numeros_impares)
